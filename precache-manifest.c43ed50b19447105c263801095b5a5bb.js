e6b7dd7908c2a2ea0e2cb6d8e5244f06a3076f7c
romanyanke <roman@yanke.ru>
2018-11-28 13:56:13 +0300

    Updates

---
+++ precache-manifest.c43ed50b19447105c263801095b5a5bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae0f85e231f2a9ed9ab5",
+    "url": "/static/css/main.9b786ccc.chunk.css"
+  },
+  {
+    "revision": "ae0f85e231f2a9ed9ab5",
+    "url": "/static/js/main.ae0f85e2.chunk.js"
+  },
+  {
+    "revision": "73c84f7e11b16f65ed81",
+    "url": "/static/js/1.73c84f7e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc51792eddfd6d23c4bea8afe3718548",
+    "url": "/static/media/romanyanke.bc51792e.jpg"
+  },
+  {
+    "revision": "e7ceae366c313931ab6a6fc2bc122b2f",
+    "url": "/static/media/romanyanke@2x.e7ceae36.jpg"
+  },
+  {
+    "revision": "4c84f2ec4c05ba25a35fed1899b765ea",
+    "url": "/static/media/romanyanke@3x.4c84f2ec.jpg"
+  },
+  {
+    "revision": "e15edcd6d18cda69844050b63ebdcb7e",
+    "url": "/index.html"
+  }
+];
